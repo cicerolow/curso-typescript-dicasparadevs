b8173371f68ca90a64514037f8f2d2496dcba116
cicerolow <ciceroqlima@gmail.com>
2023-07-30 18:31:53 -0300

    treinameno typscript

---
+++ index.js
@@ -0,0 +1,10 @@
+// Uso do combiron no typescript
+// let age = 5;
+// console.log(age);
+//UTILIZANDO RECURSO DO TYPESCRIPT
+// let age: number = 'a' // o tipo 'string' não pode ser atribuido tipo 'number' 
+// let age: number = 5
+// age = '5' // o tipo 'string' não pode ser atribuido tipo 'number'
+// console.log(age);
+var age = 5;
+console.log(age);
---
+++ index.ts
@@ -0,0 +1,41 @@
+// Uso do combiron no typescript
+
+// let age = 5;
+
+// console.log(age);
+
+//UTILIZANDO RECURSO DO TYPESCRIPT
+
+// let age: number = 'a' // o tipo 'string' não pode ser atribuido tipo 'number' 
+
+// let age: number = 5
+
+// age = '5' // o tipo 'string' não pode ser atribuido tipo 'number'
+
+// console.log(age);
+
+
+// let age: number = 5;
+
+// console.log(age);
+
+
+// TIPOS BÁSICOS
+
+let age: number = 5;
+const firstName: string = 'Felipe'
+const isValid: boolean = true
+let idk: any = 5 // any lista de qualquer coisa. Bom evitar usar
+
+idk = '12'
+idk = true
+
+const ids: number[] = [1, 2, 3, 4, 5] //lista de number
+const boolean: boolean[] = [true, false, true, false] //lista de boolean
+const names: string[] = ["Felipe", "cicero", "Maria"] //lista de nomes "strings"
+
+console.log(age);
+
+
+
+
